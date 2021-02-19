438b5e606133b32568eb21f733405a6310ed65ab
kiavash <kiavash@gmail.com>
2021-02-19 23:33:07 +0330

    edit snap maker

--- src/renderer.ts
+++ src/renderer.ts
@@ -5,29 +5,29 @@
 // Use preload.js to selectively enable features
 // needed in the renderer process.
 
+function swapStyleSheet(sheet: string): void {
+  document.getElementById('pagestyle').setAttribute('href', sheet);
+}
+
 const changeStyle = document.getElementById('changestyle') as HTMLInputElement
 changeStyle.addEventListener('change', () => {
-    const sheet: string =
-        changeStyle.checked ? "./static/darkstyle.css" : "./static/lightstyle.css";
-    swapStyleSheet(sheet)
-    const changeStyleText = document.getElementById('changestyletext')
-    changeStyleText.innerText =
-        changeStyle.checked ? "Light Theme" : "Dark Theme";
+  const sheet: string =
+    changeStyle.checked ? "./static/darkstyle.css" : "./static/lightstyle.css";
+  swapStyleSheet(sheet)
+  const changeStyleText = document.getElementById('changestyletext')
+  changeStyleText.innerText =
+    changeStyle.checked ? "Light Theme" : "Dark Theme";
 })
-
-function swapStyleSheet(sheet: string): void {
-    document.getElementById('pagestyle').setAttribute('href', sheet);
-}
 
 
 // Get the modal (help)
 const helpModal: HTMLElement = document.getElementById("helpModal");
 const helpBtn: HTMLElement = document.getElementById("helpBtn");
 const helpSpan: HTMLElement = document.getElementById("helpModalClose");
-helpBtn.onclick = function(): void {
+helpBtn.onclick = function (): void {
   helpModal.style.display = "block";
 }
-helpSpan.onclick = function(): void {
+helpSpan.onclick = function (): void {
   helpModal.style.display = "none";
 }
 
@@ -35,14 +35,14 @@ helpSpan.onclick = function(): void {
 const aboutModal: HTMLElement = document.getElementById("aboutModal");
 const aboutBtn: HTMLElement = document.getElementById("aboutBtn");
 const aboutSpan: HTMLElement = document.getElementById("aboutModalClose");
-aboutBtn.onclick = function(): void {
+aboutBtn.onclick = function (): void {
   aboutModal.style.display = "block";
 }
-aboutSpan.onclick = function(): void {
+aboutSpan.onclick = function (): void {
   aboutModal.style.display = "none";
 }
 
-window.onclick = function(event: Event): void {
+window.onclick = function (event: Event): void {
   if (event.target === helpModal) {
     helpModal.style.display = "none";
   }

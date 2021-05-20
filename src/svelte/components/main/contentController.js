a39e25b95f6aa7ecd89a58e29b206448cdf6b109
kiavash <kiavash@gmail.com>
2021-05-20 22:09:11 +0430

    fix first aya bug

--- src/svelte/components/main/contentController.js
+++ src/svelte/components/main/contentController.js
@@ -2,12 +2,13 @@
 import { Page, rtlLangs } from "../../quran-data";
 
 export function contentController(textSelected, translateSelected, pageNumber, ayaNumberInQuran) {
-    const text = require(`./assets/text/${textSelected}.js`)
-    const translate = require(`./assets/translate/${translateSelected}.js`)
+    const text = require(`./assets/text/${textSelected}.js`);
+    const translate = require(`./assets/translate/${translateSelected}.js`);
 
     const match = /(\w+)_\w+/.exec(translateSelected);
     const rtl = rtlLangs.includes(match[1]) ? true : false;
     let data = [];
+	let textContent;
     for (
         let ayaNumInQuran = Page[pageNumber][2];
         ayaNumInQuran < Page[pageNumber + 1][2];
@@ -16,7 +17,7 @@ export function contentController(textSelected, translateSelected, pageNumber, a
         const suraNum = text[ayaNumInQuran][1] - 1
         const ayaNumInSura = text[ayaNumInQuran][2]
         if (ayaNumInSura === 1 && suraNum !== 0 && suraNum !== 8)
-            text[ayaNumInQuran][3] = text[ayaNumInQuran][3].replace(
+            textContent = text[ayaNumInQuran][3].replace(
                 /^(([^ ]+ ){4})/u,
                 ""
             );
@@ -24,7 +25,7 @@ export function contentController(textSelected, translateSelected, pageNumber, a
             ayaNumInQuran,
             suraNum,
             ayaNumInSura,
-            text: text[ayaNumInQuran][3],
+            text: textContent,
             translate: translate[ayaNumInQuran][3],
             rtl: rtl,
             selected: ayaNumInQuran === ayaNumberInQuran ? true : false,
